0033a026ed570a143dde784d653ea9857c95d9fd
wicksa <31991330+wicksa-art@users.noreply.github.com>
2023-08-06 00:14:45 +0300

    Update closure.py

--- work2/closure.py
+++ work2/closure.py
@@ -11,12 +11,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 # Make sure a command line argument was given
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
     print("Please provide a search term as a command line argument.")
     sys.exit()
 
-email_term = sys.argv[1]
-search_term = sys.argv[2]
+search_term = sys.argv[1]
 
 options = Options()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -30,6 +29,8 @@ driver = webdriver.Chrome(service=service, options=options)
 #driver.maximize_window()
 
 driver.get('https://core.altbetexchange.com/core/#/login/staff')
+
+found_items = [] # Store detected items here
 
 try:
     username_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'username')))
@@ -47,38 +48,7 @@ try:
     search_player_link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[routerlink="/app/core/players/search"]')))
     search_player_link.click()
 
-    time.sleep(1)
-
-    # Clicking on the dropdown
-    dropdown_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.dropdown-toggle.btn.btn-default.btn-block.filter-dropdown')))
-    dropdown_button.click()
-
-    # Clicking on the span with Email text
-    email_span = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Email"]')))
-    email_span.click()
-
-    search_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[autofocus="true"]')))
-    search_field.click()
-    search_field.send_keys(email_term)
-
-    # Wait for the button to be clickable and click it
-    success_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.btn.btn-sm.btn-success')))
-    success_button.click()
-
-    # Attempt to click on player_link
-    player_link = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.player-link')))
-    player_link.click()
-
-except (NoSuchElementException, TimeoutException):
-    # If not able to click on player_link, execute this:
-    # Clicking on the dropdown
-    dropdown_button = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.dropdown-toggle.btn.btn-default.btn-block.filter-dropdown')))
-    dropdown_button.click()
-
-    # Clicking on the span with Email text
-    email_span = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, '//span[text()="User Id"]')))
-    email_span.click()
-
+    #insert
     # Clicking on the dropdown
     dropdown_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Contains") and contains(@class, "dropdown-toggle") and contains(@class, "btn") and contains(@class, "btn-default") and contains(@class, "btn-block") and contains(@class, "filter-dropdown")]')))
     dropdown_button.click()
@@ -87,33 +57,36 @@ except (NoSuchElementException, TimeoutException):
     email_span = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Equals"]')))
     email_span.click()
 
-    search_field = WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[autofocus="true"]')))
+    # Wait for the input field to be clickable, enter the search term into it
+    search_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[autofocus="true"]')))   
     search_field.click()
-    search_field.clear()
     search_field.send_keys(search_term)
-
+    
     # Wait for the button to be clickable and click it
     success_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.btn.btn-sm.btn-success')))
     success_button.click()
 
-    time.sleep(1)
-
+    # Wait for the player link to be clickable and click it
     player_link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.player-link')))
     player_link.click()
 
-    first_div = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'mat-tab-label-2-9')))
-    first_div.click()
+    time.sleep(1)
 
-    second_div = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//i[contains(@class, "fa fa-question-circle")]/following::div[contains(@class, "ng-star-inserted")][1]')))
-    second_div.click()
+    element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//td[contains(@class, 'text-xs-right') and contains(., 'NOT_LOCKED')]/following-sibling::td"))
+        )
+        # Find the i element within this td and click it
+    i_element = element.find_element_by_class_name('fa-pencil-square-o')
+    i_element.click()
 
-    email_change = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'profileEmail')))
-    email_change.clear()
-    email_change.send_keys(email_term)
+    element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'btn') and contains(@class, 'btn-secondary') and contains(@class, 'filter-dropdown')]"))
+    )
+    element.click()
 
 except NoSuchElementException:
     print("Element not found on the page.")
 except TimeoutException:
     print("Timeout while waiting for the elements to load on the page.")
 
-time.sleep(360)  # wait for 100000 seconds before closing
+time.sleep(60)  # wait for 100000 seconds before closing
